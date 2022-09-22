067c775109b9b0b7e6c47681ff0d366fe5e1fc64
vjetcong <hoanghcm188@gmail.com>
2022-09-22 09:30:40 +0700

    calc

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -98,6 +98,40 @@ function getKeyByValueMap(map, searchValue) {
 	}
 }
 
+function generateComponents() {
+	const row1 = new MessageActionRow().addComponents(
+		new MessageButton().setCustomID('clear').setLabel('C').setStyle('DANGER'),
+		new MessageButton().setCustomID('(').setLabel('(').setStyle('PRIMARY'),
+		new MessageButton().setCustomID(')').setLabel(')').setStyle('PRIMARY'),
+		new MessageButton().setCustomID('^').setLabel('^').setStyle('PRIMARY')
+	);
+	const row2 = new MessageActionRow().addComponents(
+		new MessageButton().setCustomID('7').setLabel('7').setStyle('SECONDARY'),
+		new MessageButton().setCustomID('8').setLabel('8').setStyle('SECONDARY'),
+		new MessageButton().setCustomID('9').setLabel('9').setStyle('SECONDARY'),
+		new MessageButton().setCustomID('/').setLabel('/').setStyle('PRIMARY')
+	);
+	const row3 = new MessageActionRow().addComponents(
+		new MessageButton().setCustomID('4').setLabel('4').setStyle('SECONDARY'),
+		new MessageButton().setCustomID('5').setLabel('5').setStyle('SECONDARY'),
+		new MessageButton().setCustomID('6').setLabel('6').setStyle('SECONDARY'),
+		new MessageButton().setCustomID('*').setLabel('*').setStyle('PRIMARY')
+	);
+	const row4 = new MessageActionRow().addComponents(
+		new MessageButton().setCustomID('1').setLabel('1').setStyle('SECONDARY'),
+		new MessageButton().setCustomID('2').setLabel('2').setStyle('SECONDARY'),
+		new MessageButton().setCustomID('3').setLabel('3').setStyle('SECONDARY'),
+		new MessageButton().setCustomID('-').setLabel('-').setStyle('PRIMARY')
+	);
+	const row5 = new MessageActionRow().addComponents(
+		new MessageButton().setCustomID('0').setLabel('0').setStyle('SECONDARY'),
+		new MessageButton().setCustomID('.').setLabel('.').setStyle('SECONDARY'),
+		new MessageButton().setCustomID('=').setLabel('=').setStyle('SUCCESS'),
+		new MessageButton().setCustomID('+').setLabel('+').setStyle('PRIMARY')
+	);
+	return [row1, row2, row3, row4, row5];
+}
+
 module.exports.pickRandom = pickRandom;
 module.exports.sendLoadingMessage = sendLoadingMessage;
 module.exports.returnContentForSlashOrSendMessage = returnContentForSlashOrSendMessage;
@@ -105,3 +139,4 @@ module.exports.returnSlashAndMessage = returnSlashAndMessage;
 module.exports.smallNumberDisplay = smallNumberDisplay;
 module.exports.sendCaptchaImage = sendCaptchaImage;
 module.exports.getKeyByValueMap = getKeyByValueMap;
+module.exports.generateComponents = generateComponents;
