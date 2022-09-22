dae354c6f1f7af172b8a2d4fbac7d289e251905d
vjetcong <hoanghcm188@gmail.com>
2022-09-22 10:05:34 +0700

    lỡ tay làm nốt rồi ngủ

--- src/commands/giveaways/start-giveaways.js
+++ src/commands/giveaways/start-giveaways.js
@@ -1,3 +1,9 @@
+const { send } = require('@sapphire/plugin-editable-commands');
+const { fetchT } = require('@sapphire/plugin-i18next');
+const { MessageEmbed } = require('discord.js');
+const WynnCommand = require('../../lib/Structures/WynnCommand');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
 class UserCommand extends WynnCommand {
 	constructor(context, options) {
 		super(context, {
@@ -10,4 +16,37 @@ class UserCommand extends WynnCommand {
 			//cooldownDelay: 5000
 		});
 	}
+    async messageRun (client, message, args) {
+
+        if(!message.member.permissions.has('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
+            return message.reply({
+                content: '',
+                ephemeral: true
+            });
+        }
+    
+        const giveawayChannel = null;
+        const giveawayDuration = null;
+        const giveawayWinnerCount = null;
+        const giveawayPrize = null;
+        
+        if(!giveawayChannel.isTextBased()) {
+            return message.reply({
+                content: '',
+                ephemeral: true
+            });
+        }
+    
+        client.giveawaysManager.start(giveawayChannel, {
+            duration: ms(giveawayDuration),
+            prize: giveawayPrize,
+            winnerCount: giveawayWinnerCount,
+            hostedBy: client.config.hostedBy ? message.user : null,
+            messages
+        });
+    
+        message.reply(`Giveaway started in ${giveawayChannel}!`);
+    
+    } 
+
 }
